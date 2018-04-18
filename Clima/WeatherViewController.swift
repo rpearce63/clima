c70665b0e9bd39a598e9777294df25cf9a84f367
Rick Pearce <rick.pearce.crzs@statefarm.com>
2018-04-18 15:58:55 -0500

    change locations, and add f/c switch

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -11,7 +11,7 @@ import CoreLocation
 import Alamofire
 import SwiftyJSON
 
-class WeatherViewController: UIViewController, CLLocationManagerDelegate {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate, ChangeCityDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
@@ -26,7 +26,8 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var weatherIcon: UIImageView!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
-
+    @IBOutlet weak var modeToggle: UISegmentedControl!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,7 +51,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         
         Alamofire.request(url, method: .get, parameters: parameters).responseJSON { (response) in
             if response.result.isSuccess {
-                print("Success! Got the weather data")
+                //print("Success! Got the weather data")
                 
                 let weatherJSON: JSON = JSON(response.result.value!)
                 
@@ -76,7 +77,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     //Write the updateWeatherData method here:
     func updateWeatherData(json: JSON) {
         if let tempResult = json["main"]["temp"].double {
-            weatherDataModel.temperature = Int(tempResult * (9 / 5) - 459.67)
+            weatherDataModel.temperature = Int(tempResult)
             weatherDataModel.city = json["name"].stringValue
             weatherDataModel.condition = json["weather"][0]["id"].intValue
             weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
@@ -97,7 +98,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     //Write the updateUIWithWeatherData method here:
     func updateUIWithWeatherData() {
         cityLabel.text = weatherDataModel.city
-        temperatureLabel.text = String(weatherDataModel.temperature)
+        temperatureLabel.text = "\(formatTemperature(tempK: weatherDataModel.temperature))°"
         weatherIcon.image = UIImage(named: weatherDataModel.weatherIconName)
         
     }
@@ -142,14 +143,45 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     
     //Write the userEnteredANewCityName Delegate method here:
-    
+    func userEnteredANewCityName(city: String) {
+        
+        let params : [String : String] = ["q" : city, "appId" : APP_ID]
+        getWeatherData(url: WEATHER_URL, parameters: params)
+    }
 
     
     //Write the PrepareForSegue Method here
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "changeCityName" {
+            let destinationVC = segue.destination as! ChangeCityViewController
+            destinationVC.delegate = self
+        }
+    }
     
     
+    @IBAction func degreeModeBtnPressed(_ sender: UISegmentedControl) {
+        updateUIWithWeatherData()
+        //temperatureLabel.text = "\(formatTemperature(tempK: weatherDataModel.temperature))°"
+    }
     
+    private func formatTemperature(tempK: Int) -> Int {
+        if modeToggle.selectedSegmentIndex == 0 {
+            return convertToFarenheit(kelvin: weatherDataModel.temperature)
+        } else {
+            return convertToCelcius(kelvin: weatherDataModel.temperature)
+        }
+    }
+    
+    private func convertToFarenheit(kelvin: Int) -> Int {
+        let degF = Double(kelvin) * (9/5) - 459.67
+        return Int(degF)
+    }
+    
+    private func convertToCelcius(kelvin: Int) -> Int {
+        let degC = Double(kelvin) - 273.15
+        return Int(degC)
+    }
     
 }
 
